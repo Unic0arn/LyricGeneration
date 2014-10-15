dbd6fab6f7d972827c76ffa93d87beeeb6b18e3a
Fredrik Hallberg <fhallb@kth.se>
2014-10-15 09:51:33 +0200

    init

---
+++ LyricGeneration/src/LyricGeneration.java
@@ -0,0 +1,14 @@
+
+public class LyricGeneration {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public LyricGeneration(){
+		
+	}
+}
