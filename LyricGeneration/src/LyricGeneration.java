107b33b0b095221951e89f51741b4d42f1172d4d
Joakim Candefors <candef@kth.se>
2014-10-19 16:36:19 +0200

    fixed bugs in getNextWord and calcBigrams
    fixed backtracking! added 0-prob-check for next word and fixed
    calcBigrams to actually calc the whole, all words in a, row

--- LyricGeneration/src/LyricGeneration.java
+++ LyricGeneration/src/LyricGeneration.java
@@ -17,38 +17,48 @@ import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 public class LyricGeneration {
 	final static String  corpusName = "resources/corpus";
+	private static final boolean debug = false;
 	//final static String[] POSTags = {"CC","CD","DT","EX","FW","IN","JJ","JJR","JJS","LS","MD","NN","NNS","NNP","NNPS","PDT","POS","PRP","PRP$","RB","RBR","RBS","RP","SYM","TO","UH","VB","VBD","VBG","VBN","VBP","VBZ","WDT","WP","WP$","WRB","RS","RB"};
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		MaxentTagger tagger = new MaxentTagger(
 				"taggers/english-left3words-distsim.tagger");
 		ArrayList<String> lines = readCorpus(corpusName);
 		ArrayList<String> posTaggedLines = tagLines(lines, tagger);
 		ArrayList<String> posTemplates = createPosTemplates(posTaggedLines);
 		// Output the POS Templates
-		
-		//System.err.println(Arrays.toString(posTemplates.toArray()));
-		
+
+		if(debug)System.err.println(Arrays.toString(posTemplates.toArray()));
+
 		Word[] uniqueWordList = getUniqueWords(posTaggedLines);
 		String[] POSTags = getUniquePOSTags(posTaggedLines);
 		Arrays.sort(POSTags);
 		Arrays.sort(uniqueWordList); // Sort by POS tag
-		/*for (int i = 0; i < uniqueWordList.length; i++) {
+		/*
+			for (int i = 0; i < uniqueWordList.length; i++) {
+
 			System.out.println(i + " "+ uniqueWordList[i].word +" : " + uniqueWordList[i].POS );			
-		}*/
+		}
+		 */
 		int[] indexes = prepareIndexes(uniqueWordList, POSTags);
-				
+
 		int SOL = indexes[Arrays.binarySearch(POSTags, "SOL")];
 		int EOL = indexes[Arrays.binarySearch(POSTags, "EOL")];
-		
-		double[][] bigrams = calcBigrams(posTaggedLines, uniqueWordList, POSTags, indexes, SOL, EOL);
-		byte[][][] trigrams = calcTrigrams(posTaggedLines, uniqueWordList, POSTags, indexes, SOL, EOL);
-		String verserow = synthesizeRowTriGram(posTemplates, uniqueWordList, POSTags,indexes, SOL, trigrams);
+
+		// for bigrams:
+		double[][] bigrams = calcBigrams(posTaggedLines, uniqueWordList, POSTags, indexes, SOL, EOL);		
+		String verserow = synthesizeRow(posTemplates, uniqueWordList, POSTags,indexes, SOL, bigrams);
+
+		//for trigrams:
+		//byte[][][] trigrams = calcTrigrams(posTaggedLines, uniqueWordList, POSTags, indexes, SOL, EOL);
+		//String verserow = synthesizeRowTriGram(posTemplates, uniqueWordList, POSTags,indexes, SOL, trigrams);
+
 		System.out.println(verserow);
 		/*
+		//for generating whole song:
 		for (int i = 0; i < 4; i++) {
 			String verserow = synthesizeRow(posTemplates, uniqueWordList, POSTags,indexes, SOL, bigrams);
 			System.out.println(verserow);
@@ -73,9 +83,9 @@ public class LyricGeneration {
 			String verserow = synthesizeRow(posTemplates, uniqueWordList, POSTags,indexes, SOL, bigrams);
 			System.out.println(verserow);
 		}
-		
-		
-		*/
+
+
+		 */
 		return;
 	}
 
@@ -89,44 +99,24 @@ public class LyricGeneration {
 				Word a = new Word(words[j]);
 				Word b = new Word(words[j+1]);
 				Word c = new Word(words[j+2]);
-					int aIndex = findWordIndex(uniqueWordList, POSTags, indexes, a);
-					int bIndex = findWordIndex(uniqueWordList, POSTags, indexes, b);
-					int cIndex = findWordIndex(uniqueWordList, POSTags, indexes, c);
+				int aIndex = findWordIndex(uniqueWordList, POSTags, indexes, a);
+				int bIndex = findWordIndex(uniqueWordList, POSTags, indexes, b);
+				int cIndex = findWordIndex(uniqueWordList, POSTags, indexes, c);
 
-					triGram[aIndex][bIndex][cIndex]++;
-					
-					if(j==0){
-						triGram[SOL][SOL][aIndex]++;
-					}else if(j==1){
-							triGram[SOL][aIndex][bIndex]++;
-					}else if(j==words.length-2){
-						triGram[bIndex][cIndex][EOL]++;
-					}else if(j==words.length-2){
-						triGram[cIndex][EOL][EOL]++;
-					}
-				
+				triGram[aIndex][bIndex][cIndex]++;
+
+				if(j==0){
+					triGram[SOL][SOL][aIndex]++;
+				}else if(j==1){
+					triGram[SOL][aIndex][bIndex]++;
+				}else if(j==words.length-2){
+					triGram[bIndex][cIndex][EOL]++;
+				}else if(j==words.length-2){
+					triGram[cIndex][EOL][EOL]++;
+				}
+
 			}
 		}
-		//Normalize rows
-		/*
-		byte[][] sums = new double[triGram.length][triGram.length];
-		for (int i = 0; i < triGram.length; i++) {
-			for (int j = 0; j < triGram.length; j++) {
-				double rowSum = 0.0;
-				for (int k = 0; k < triGram.length; k++) {
-					rowSum += triGram[i][j][k];
-				}
-
-				sums[i][j] = rowSum;
-				rowSum = 1/rowSum;
-				for (int k = 0; k < triGram.length; k++) {
-					triGram[i][j][k] = triGram[i][j][k] * rowSum;
-					if(Double.isNaN(triGram[i][j][k])){
-						System.err.println("lol2");
-					}
-				}
-			}
-		}*/
 		return triGram;
 	}
 
@@ -140,25 +130,29 @@ public class LyricGeneration {
 	 * @return
 	 */
 	private static String synthesizeRow(ArrayList<String> posTemplates,
-		Word[] uniqueWordList, String[] POSTags, int[] indexes, int SOL,
-		double[][] bigrams) {
+			Word[] uniqueWordList, String[] POSTags, int[] indexes, int SOL,
+			double[][] bigrams) {
 		Random r = new Random();
 		String[] posTemplate = posTemplates.get(r.nextInt(posTemplates.size())).split(" ");		
-		//System.out.println(Arrays.toString(posTemplate));
+		if(debug)System.out.println(Arrays.toString(posTemplate));
 		StringBuilder sb = new StringBuilder();
 		int indexOfPT = 0;
 		int prevWord = SOL; 
 		ArrayList<Integer> row = new ArrayList<Integer>();
-		
-			row = getNextWord(posTemplate , indexOfPT, bigrams, prevWord, POSTags, indexes);
-		
-		
+
+		row = getNextWord(posTemplate , indexOfPT, bigrams, prevWord, POSTags, indexes);
+
+		//if no match against template (should not happen)
+		if(row == null){
+			synthesizeRow(posTemplates, uniqueWordList, POSTags, indexes, SOL, bigrams);
+		}
+
 		for (int i = row.size(); i > 0; i--) {			
 			sb.append(uniqueWordList[row.get(i-1)].toString2()+" ");			
 		}			
-		
+
 		return sb.toString();
-	
+
 	}
 
 	private static ArrayList<Integer> getNextWord(String[] posTemplate, int indexOfPT, double[][] bigrams, int prevWord, String[] POSTags, int[] indexes) {
@@ -174,11 +168,18 @@ public class LyricGeneration {
 		}
 		Arrays.sort(flwWordProbs);
 		for (int i = 0; i < flwWordProbs.length; i++) {
+			
+			if(flwWordProbs[i].prob == 0.0){
+				//if no chance of word - break (since array is sorted)
+				break;
+			}
+			if(debug)System.err.println("index of POStemplate = " + indexOfPT + " trying with word: "+flwWordProbs[i].index + " with prob: " + flwWordProbs[i].prob);
 			ArrayList<Integer> nextWordindex = getNextWord(posTemplate, indexOfPT+1, bigrams, flwWordProbs[i].index, POSTags, indexes);
 			if(nextWordindex == null){
 				continue;
 			}else{
 				nextWordindex.add(flwWordProbs[i].index);
+				if(debug)System.err.println("index of POStemplate = " + indexOfPT + " words added: " + Arrays.toString(nextWordindex.toArray()));
 				return nextWordindex;
 			}
 		}
@@ -187,20 +188,20 @@ public class LyricGeneration {
 	private static String synthesizeRowTriGram(ArrayList<String> posTemplates,
 			Word[] uniqueWordList, String[] POSTags, int[] indexes, int SOL,
 			byte[][][] trigrams) {
-			Random r = new Random();
-			String[] posTemplate = posTemplates.get(r.nextInt(posTemplates.size())).split(" ");		
-			//System.out.println(Arrays.toString(posTemplate));
-			StringBuilder sb = new StringBuilder();
-			int indexOfPT = 0;
-			int prevWord = SOL; 
-			ArrayList<Integer> row = new ArrayList<Integer>();
-			row = getNextWordTriGram(posTemplate , indexOfPT, trigrams, prevWord,prevWord, POSTags, indexes);
-			for (int i = row.size(); i > 0; i--) {			
-				sb.append(uniqueWordList[row.get(i-1)].toString2()+" ");			
-			}			
-			return sb.toString();
-		}
-	
+		Random r = new Random();
+		String[] posTemplate = posTemplates.get(r.nextInt(posTemplates.size())).split(" ");		
+		if(debug)System.out.println(Arrays.toString(posTemplate));
+		StringBuilder sb = new StringBuilder();
+		int indexOfPT = 0;
+		int prevWord = SOL; 
+		ArrayList<Integer> row = new ArrayList<Integer>();
+		row = getNextWordTriGram(posTemplate , indexOfPT, trigrams, prevWord,prevWord, POSTags, indexes);
+		for (int i = row.size(); i > 0; i--) {			
+			sb.append(uniqueWordList[row.get(i-1)].toString2()+" ");			
+		}			
+		return sb.toString();
+	}
+
 	private static ArrayList<Integer> getNextWordTriGram(String[] posTemplate, int indexOfPT, 
 			byte[][][] trigrams, int prevPrevWord, int prevWord,  String[] POSTags, int[] indexes) {
 		if(indexOfPT == posTemplate.length){
@@ -238,20 +239,27 @@ public class LyricGeneration {
 		double[][] biGram = new double[uniqueWordList.length][uniqueWordList.length];
 		for (String ptl : posTaggedLines) {
 			String[] words = ptl.split(" ");
-			for (int j = 0; j < words.length-1; j++) {
-				Word a = new Word(words[j]);
-				Word b = new Word(words[j+1]);
-					int aIndex = findWordIndex(uniqueWordList, POSTags, indexes, a);
-					int bIndex = findWordIndex(uniqueWordList, POSTags, indexes, b);
+			Word a;
+			Word b;
+			for (int j = 0; j < words.length; j++) {
 
+
+				a = new Word(words[j]);
+
+				int aIndex = findWordIndex(uniqueWordList, POSTags, indexes, a);
+				//if first word in row
+				if(j==0){
+					biGram[SOL][aIndex]++;
+				}
+				if(j < words.length-1){
+					//if have next word in row
+					b = new Word(words[j+1]);
+					int bIndex = findWordIndex(uniqueWordList, POSTags, indexes, b);
 					biGram[aIndex][bIndex]++;
-					
-					if(j==0){
-						biGram[SOL][aIndex]++;
-					}else if(j==words.length-2){
-						biGram[bIndex][EOL]++;
-					}
-				
+				}else{
+					//last word. add EOL as next word.
+					biGram[aIndex][EOL]++;
+				}				
 			}
 		}
 		//Normalize rows
@@ -263,11 +271,11 @@ public class LyricGeneration {
 			}
 			sums[i] = rowSum;
 			rowSum = 1/rowSum;
-			
+
 			for (int j = 0; j < biGram.length; j++) {
 				biGram[i][j] = biGram[i][j] * rowSum;
-				if(Double.isNaN(biGram[i][j])){
-					//System.err.println("lol2");
+				if(debug && i != 22 && Double.isNaN(biGram[i][j])){
+					System.err.println("Nan found");
 				}
 			}
 		}
@@ -289,7 +297,7 @@ public class LyricGeneration {
 		int aIndex = binary_search(uniqueWordList, a, aMin, aMax);
 		return aIndex;
 	}
-	
+
 	private static String[] getUniquePOSTags(ArrayList<String> posTaggedLines) {
 		HashSet<String> posList = new HashSet<String>();
 		posList.add("SOL");
@@ -320,7 +328,7 @@ public class LyricGeneration {
 		for (int i = 0; i < wordList.length; i++) {
 			Word curWord = wordList[i];
 			if(!currentPOSTag.equals(curWord.POS)){
-				//System.err.println("Going to next POSTAG " + currentPOSTag + " -> " + curWord.POS + " at index: " + i);
+				if(debug)System.err.println("Going to next POSTAG " + currentPOSTag + " -> " + curWord.POS + " at index: " + i);
 				currentPOSTag = curWord.POS;
 				indexes[POSTag] = i;
 				POSTag++;
@@ -328,27 +336,27 @@ public class LyricGeneration {
 		}
 		return indexes;
 	}
-	
-	
+
+
 	private static int binary_search(Word[] wordList, Word word, int imin, int imax){
-	  if (imax < imin){
-		  return -1;
-	  }else{
-	      // calculate midpoint to cut set in half
-	      int imid = (imin + imax) /2;
-	      int comparison = wordList[imid].compareTo(word);
-	    		  
-	      // three-way comparison
-	      if (comparison > 0)
-	        // key is in lower subset
-	        return binary_search(wordList, word, imin, imid-1);
-	      else if (comparison < 0)
-	        // key is in upper subset
-	        return binary_search(wordList, word, imid+1, imax);
-	      else
-	        // key has been found
-	        return imid;
-	    }
+		if (imax < imin){
+			return -1;
+		}else{
+			// calculate midpoint to cut set in half
+			int imid = (imin + imax) /2;
+			int comparison = wordList[imid].compareTo(word);
+
+			// three-way comparison
+			if (comparison > 0)
+				// key is in lower subset
+				return binary_search(wordList, word, imin, imid-1);
+			else if (comparison < 0)
+				// key is in upper subset
+				return binary_search(wordList, word, imid+1, imax);
+			else
+				// key has been found
+				return imid;
+		}
 	}
 	private static Word[] getUniqueWords(ArrayList<String> posTaggedLines) {
 		HashSet<Word> wordList = new HashSet<Word>();
@@ -366,7 +374,7 @@ public class LyricGeneration {
 	}
 	private static ArrayList<String> createPosTemplates(ArrayList<String> posTaggedLines) {
 		ArrayList<String> posTemplates = new ArrayList<String>();
-		
+
 		HashMap<String, Integer> POSTemplateOccurances = new HashMap<String, Integer>();
 		for (String taggedLine : posTaggedLines) {
 			taggedLine = taggedLine.replaceAll("['\\w]+_|\\s$", ""); //Remove the actual words leaving only the POS-tags
@@ -409,8 +417,9 @@ public class LyricGeneration {
 		}
 		return lines;
 	}
-	
+
 }
+
 class WordProb implements Comparable<WordProb>{
 
 	double prob;
@@ -421,11 +430,10 @@ class WordProb implements Comparable<WordProb>{
 		this.index = index;
 	}
 	@Override
-	public int compareTo(WordProb o) {
-		// TODO Auto-generated method stub
+	public int compareTo(WordProb o) {		
 		return Double.compare(o.prob, prob);
 	}
-	
+
 	@Override
 	public String toString(){
 		return index + " : " + prob ;
@@ -441,15 +449,14 @@ class Word implements Comparable<Word>{
 		String[] tokens = word.split("_");
 		if(tokens.length != 2) throw new IllegalArgumentException();
 		if(tokens[0].equals("'")){
-			System.err.println("lol");
+			System.err.println("found ' in corpus");
 		}
 		original = word;
 		this.word = tokens[0];
 		this.POS = tokens[1];
 	}
-	
-	public Object toString2() {
-		// TODO Auto-generated method stub
+
+	public Object toString2() {		
 		return word;
 	}
 
@@ -462,8 +469,7 @@ class Word implements Comparable<Word>{
 		return (POS+" "+word).compareTo(o.POS +" "+ o.word);
 	}
 	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
+	public int hashCode() {		
 		if(original == null) return (word + POS).hashCode();
 		return original.hashCode();
 	}
@@ -471,8 +477,7 @@ class Word implements Comparable<Word>{
 		return word + ":" + POS + " " + hashCode();
 	}
 	@Override
-	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
+	public boolean equals(Object obj) {		
 		return hashCode() == obj.hashCode();
 	}
 }
